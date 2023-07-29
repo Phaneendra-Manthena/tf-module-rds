17dd561499137615e9a698a3ddd154aef3bbc10f
phaneendra <phani.manthena27@gmail.com>
2023-07-29 10:15:58 +0530

    updating ./tf-module-rds

--- main.tf
+++ main.tf
@@ -43,6 +43,14 @@ resource "aws_rds_cluster" "rds" {
   storage_type              = "io1"
   allocated_storage         = 20
   iops                      = 1000
-  master_username           = "test"
-  master_password           = "mustbeeightcharaters"
+  master_username           = data.aws_ssm_parameter.DB_ADMIN_USER
+  master_password           = data.aws_ssm_parameter.DB_ADMIN_PASS
+  db_subnet_group_name = aws_db_subnet_group.default.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  storage_encrypted = true
+  kms_key_id = data.aws_kms_key.key.id
+  tags = merge(
+    local.common_tags,
+    { Name =  "${var.env}-rds" }
+  )
 }
